bf9b9b37f91c3c7cde2db69c1515b208d59f1f8c
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-17 20:50:49 -0700

    finished model.py

--- model.py
+++ model.py
@@ -6,26 +6,29 @@ from flask_sqlalchemy import SQLAlchemy
 # User: required values = email, postal_code, role (default='user'), authorized (default=True)
 # Venues: indexed by 'name'
 # Venues: required values = name, category, postal_code, country (default='United States')
-# Events: indexed by ''
-# To create a user: only need an email
-#
+# Events: indexed by 'begin_at'
+# Events: required values = all except 'max_cap'
+# To create a user: only need an email and postal code
 
-# Emails + Events = Events + Yes + Invited
-# Once a user marks 'Yes' for attending, they will be added to 'Invited' table
+# Emails + Events = Invited, Interested, Confirmed
+
+# Once a user marks 'Yes' for attending, they will be added to 'Confirmed' table
 # Does not need to be a user to mark 'Yes'
-# Interested = Events + Email (if marked 'Maybe')
+# Interested = if marked 'Maybe')
 # Does not need to be a user to mark 'Maybe'
+# Invited = if added to invite list
+
+# interested || invited < confirmed
 
 db = SQLAlchemy()
 
 
-class Users(db.Model):
+class User(db.Model):
     """Users model."""
 
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, unique=True)
-
     username = db.Column(db.String(22), unique=True, nullable=True)
     password = db.Column(db.String(22), nullable=True)
     fname = db.Column(db.String(22), nullable=True)
@@ -33,12 +36,10 @@ class Users(db.Model):
     email = db.Column(db.String(100), unique=True, index=True, nullable=False)
     postal_code = db.Column(db.String(12), nullable=False)
     phone = db.Column(db.String(15), unique=True, nullable=True)
-
     role = db.Column(db.String(15), default='user', nullable=False) #permissions: admin, host, user
     authorized = db.Column(db.Boolean, default=True, nullable=False) #security: protect against blocked users
 
     def __repr__(self):
-
         repr_str = '<User: \
                     id={} \
                     username={} \
@@ -52,27 +53,25 @@ class Users(db.Model):
                     authorized={}>'
 
         return repr_str.format(
-                        self.id,
-                        self.username,
-                        self.password,
-                        self.fname,
-                        self.lname,
-                        self.email,
-                        self.postal_code,
-                        self.phone,
-                        self.role,
-                        self.authorized)
+                    self.id,
+                    self.username,
+                    self.password,
+                    self.fname,
+                    self.lname,
+                    self.email,
+                    self.postal_code,
+                    self.phone,
+                    self.role,
+                    self.authorized)
 
 
-class Venues(db.Model):
+class Venue(db.Model):
     """Venues model."""
 
     __tablename__ = 'venues'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, unique=True)
-
-    category = db.Column(db.String(22), nullable=False) #User prompts info in form: house, bar, park, nightclub, etc.
-
+    category = db.Column(db.String(22), db.ForeignKey('categories_subcategories.id')) #User prompts info in form: house, bar, park, nightclub, etc.
     name = db.Column(db.String(50), unique=True, index=True, nullable=False)
     addr_1 = db.Column(db.String(100), unique=True, nullable=True)
     addr_2 = db.Column(db.String(100), unique=True, nullable=True)
@@ -83,7 +82,6 @@ class Venues(db.Model):
 
 
     def __repr__(self):
-
         repr_str = '<User: \
                     id={} \
                     category={} \
@@ -96,24 +94,37 @@ class Venues(db.Model):
                     country={}>'
 
         return repr_str.format(
-                        self.id,
-                        self.category
-                        self.name,
-                        self.addr_1,
-                        self.addr_2,
-                        self.city,
-                        self.postal_code,
-                        self.state,
-                        self.country)
+                    self.id,
+                    self.category
+                    self.name,
+                    self.addr_1,
+                    self.addr_2,
+                    self.city,
+                    self.postal_code,
+                    self.state,
+                    self.country)
 
 
 class Category(db.Model):
-    """Categories that users can choose from for events, venues, or tpics"""
+    """Categories that users can choose from for venues"""
 
     __tablename__ = 'categories'
-    #id
-    #name
-    #image
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, unique=True)
+    name = db.Column(db.String(50), nullable=False)
+    img = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        repr_str = '<Category: \
+                    id={}, \
+                    name={}, \
+                    img={}>'
+
+
+        return repr_str.format(
+                    self.id,
+                    self.name,
+                    self.img)
 
 
 class Category_Subcategory(db.Model):
@@ -121,28 +132,39 @@ class Category_Subcategory(db.Model):
 
     __tablename__ = 'categories_subcategories'
 
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, unique=True)
+    main_category = db.Column(db.String(50), db.ForeignKey('categories.id'))
+    name = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        repr_str = '<Category_Subcategory: \
+                    id={}, \
+                    main_category={}, \
+                    name={}>'
 
 
+        return repr_str.format(
+                    self.id,
+                    self.main_category,
+                    self.name)
 
 
-class Events(db.Model):
+class Event(db.Model):
     """Events model."""
 
     __tablename__ = 'events'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, unique=True)
-
     private = db.Column(db.Boolean, default=False, nullable=False)
     host_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     venue = db.Column(db.String(50), db.ForeignKey('venues.id')) #Form prompts adding venue or searching for venue before creating event
     title = db.Column(db.String(100), nullable=False)
-    begin_at = db.Column(db.DateTime, nullable=False)
+    begin_at = db.Column(db.DateTime, nullable=False, index=True)
     end_at = db.Column(db.DateTime, nullable=False)
     max_cap = db.Column(db.String(50), nullable=True)
+    url = db.Column(db.String(100), unique=True, nullable=False)
 
-
-        def __repr__(self):
-
+    def __repr__(self):
         repr_str = '<User: \
                     id={} \
                     private={} \
@@ -151,29 +173,96 @@ class Events(db.Model):
                     title={} \
                     begin_at={} \
                     end_at={} \
-                    max_cap={}'
+                    max_cap={} \
+                    url={}'
 
         return repr_str.format(
-                        self.id,
-                        self.private,
-                        self.host_id,
-                        self.venue,
-                        self.title,
-                        self.begin_at,
-                        self.end_at,
-                        self.max_cap)
+                    self.id,
+                    self.private,
+                    self.host_id,
+                    self.venue,
+                    self.title,
+                    self.begin_at,
+                    self.end_at,
+                    self.max_cap
+                    self.url)
 
 class Invited(db.Model):
-    """Events users have RSVP'd or confirmed 'Yes'"""
+    """Events users have been invited to an event"""
 
-    __tablename__ = 'invited'
+    __tablename__ = 'invites'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, unique=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    event_id = db.Column(db.Integer, db.ForeignKey('events.id'))
+    invited_at = db.Column(db.DateTime, nullable=False)
+
+    user = db.relationship('Users', backref='invites')
+
+    def __repr__(self):
+        repr_str = '<Invited: \
+                    id={}, \
+                    user_id={}, \
+                    event_id={}, \
+                    invited_at={}>'
+
+        return repr_str.format(
+                    self.id,
+                    self.user_id,
+                    self.event_id,
+                    self.invited_at)
 
 
 class Interested(db.Model):
     """Events users have marked 'Maybe'"""
 
-    __tablename__ = 'interested'
+    __tablename__ = 'interests'
 
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, unique=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    event_id = db.Column(db.Integer, db.ForeignKey('events.id'))
+    interested_at = db.Column(db.DateTime, nullable=False)
+
+    user = db.relationship('Users', backref='interests')
+
+    def __repr__(self):
+        repr_str = '<Interested: \
+                    id={}, \
+                    user_id={}, \
+                    event_id={}, \
+                    interested_at={}>'
+
+        return repr_str.format(
+                    self.id,
+                    self.user_id,
+                    self.event_id,
+                    self.interested_at)
+
+
+class Confirmed(db.Model):
+    """Events users have confirmed a 'Yes' response"""
+
+    __tablename__ = 'confirms'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, unique=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    event_id = db.Column(db.Integer, db.ForeignKey('events.id'))
+    confirmed_at = db.Column(db.DateTime, nullable=False)
+
+    user = db.relationship('Users', backref='confirms')
+
+    def __repr__(self):
+        repr_str = '<Confirmed: \
+                    id={}, \
+                    user_id={}, \
+                    event_id={}, \
+                    invited_at={}>'
+
+        return repr_str.format(
+                    self.id,
+                    self.user_id,
+                    self.event_id,
+                    self.invited_at)
 
 
 def init_app():
